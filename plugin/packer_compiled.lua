45574be9b6b84437db48fbdf9b91be2f51339f06
Eze <ezequiel.gonzroj@gmail.com>
2023-08-08 13:21:00 -0300

    agrego fugitive para trabajar con git

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -148,6 +148,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/eze/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "/home/eze/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
   }
 }
 
