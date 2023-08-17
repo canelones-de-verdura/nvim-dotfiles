c44afd7873c6f50972116189598ed13f262c8430
Eze <ezequiel.gonzroj@gmail.com>
2023-08-17 20:42:39 -0300

    .

--- lua/plugins/catppucin.lua
+++ lua/plugins/catppucin.lua
@@ -5,7 +5,34 @@ return {
     priority = 1000,
     config = function()
         require("catppuccin").setup {
-            flavour = "latte"
+            flavour = "latte",
+            integrations = {
+                cmp = true,
+                treesitter = true,
+                mason = true,
+                markdown = true,
+                native_lsp = {
+                    enabled = true,
+                    virtual_text = {
+                        errors = { "italic" },
+                        hints = { "italic" },
+                        warnings = { "italic" },
+                        information = { "italic" },
+                    },
+                    underlines = {
+                        errors = { "underline" },
+                        hints = { "underline" },
+                        warnings = { "underline" },
+                        information = { "underline" },
+                    },
+                    inlay_hints = {
+                        background = true,
+                    },
+                },
+                telescope = {
+                    enabled = true,
+                },
+            },
         }
         vim.cmd.colorscheme "catppuccin"
     end,
