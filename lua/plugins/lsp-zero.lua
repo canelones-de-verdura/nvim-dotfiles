691c1a241463137f65eec4cff207f393c8879242
Eze <ezequiel.gonzroj@gmail.com>
2023-08-17 20:58:38 -0300

    .

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -17,6 +17,11 @@ return {
         lsp.on_attach(function(client, bufnr)
             lsp.default_keymaps({buffer = bufnr})
         end)
+        lsp.set_server_config({
+            on_init = function(client)
+                client.server_capabilities.semanticTokensProvider = nil
+            end,
+        })
         -- (Optional) Configure lua language server for neovim
         require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
         lsp.set_sign_icons({
