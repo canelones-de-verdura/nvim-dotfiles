88736787c714aed3f5cadd705899755cbebfde80
Eze <ezequiel.gonzroj@gmail.com>
2023-10-06 18:49:15 -0300

    cambié las bindings de telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,9 +4,9 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.oldfiles, {})
-        vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>h', builtin.oldfiles, {})
+        vim.keymap.set('n', '<leader>?', builtin.help_tags, {})
     end,
 }
