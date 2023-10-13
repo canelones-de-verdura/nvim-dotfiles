03e97c9ed5ad3f8e47039624c5ba95ad1a37542d
Eze <eze.gonzalez@vera.com.uy>
2023-10-13 20:02:51 -0300

    ?

--- lua/eze/config.lua
+++ lua/eze/config.lua
@@ -27,14 +27,28 @@ vim.opt.background = "light"
 vim.opt.cmdheight=1
 vim.opt.laststatus = 3
 vim.opt.showtabline = 1
+vim.opt.showmode = false
 -- Netrw
-vim.cmd([[
-    let g:netrw_cursor = 0
-    let g:netrw_keepdir = 0
-    let g:netrw_liststyle = 0
-    let g:netrw_banner = 0
-    let g:netrw_winsize = 30
-    let g:netrw_localcopydircmd = 'cp -r'
-]])
+vim.g.netrw_banner = 0
+vim.g.netrw_cursor = 0
+vim.g.netrw_keepdir = 0
+vim.g.netrw_liststyle = 0
+vim.g.netrw_localcopydircmd = 'cp -r'
+-- Binds
+-- Faltan las de telescope y nvim-cmp. Esas están en plugins/.
+-- Exploro archivos con espacio+e
+vim.keymap.set("n", "<leader>e",  vim.cmd.Ex)
+-- Me muevo entre buffers
+vim.keymap.set("n", "<Tab>",  vim.cmd.bn)
+-- Cierro buffers
+vim.keymap.set("n", "<leader>c", vim.cmd.bd)
+-- Mueven lo que esté seleccionado 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Centran la vista mientras me desplazo
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Reemplaza la palabra bajo el cursor en todo el documento
+vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- Varios
 vim.opt.updatetime = 50
