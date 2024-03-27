0f9780d888cfcbc3b236c7d840e816c790b8d70d
Eze <eze.gonzalez@vera.com.uy>
2024-03-27 20:01:31 -0300

    Agrego binds para gitsigns

--- lua/configs/gitsigns.lua
+++ lua/configs/gitsigns.lua
@@ -1,1 +1,10 @@
-require('gitsigns').setup()
+-- Binds
+vim.keymap.set('n', '<leader>gb', ":Gitsigns toggle_current_line_blame<CR>")
+vim.keymap.set('n', '<leader>gp', ":Gitsigns preview_hunk<CR>")
+
+-- Cambio solo el borde de los popups, de rompe bolas nomás
+require('gitsigns').setup{
+    preview_config = {
+        border = 'rounded',
+    },
+}
