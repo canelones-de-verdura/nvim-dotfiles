b6e82dd1a632e969f86064774f3fc8c56703d135
Eze <eze.gonzalez@vera.com.uy>
2024-03-27 19:28:21 -0300

    Agrego gitsigns.nvim

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -23,7 +23,7 @@ require('lualine').setup {
     options = {
         icons_enabled = true,
         theme = tema,
-        component_separators = { left = '•', right = '•' },
+        component_separators = { left = '·', right = '·' },
         section_separators = { left = '', right = '' },
         ignore_focus = {},
         always_divide_middle = true,
