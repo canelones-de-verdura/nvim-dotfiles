63995e4d64b51d3ea964a50ffdc3e7c988a30d38
Eze <eze.gonzalez@vera.com.uy>
2024-04-25 16:04:58 -0300

    ?

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,17 +1,10 @@
 require("nvim-treesitter.configs").setup {
-    ensure_installed = {
-        "python",
-        "c",
-        "cpp",
-        "lua",
-    },
+    ensure_installed = {},
     sync_install = false,
     auto_install = true,
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
-    indent = {
-        enable = true,
-    },
+    indent = { enable = true },
 }
