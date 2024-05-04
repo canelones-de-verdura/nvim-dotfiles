559f340c57302f2c93d4f3c0044b51908fccb9f4
Eze <eze.gonzalez@vera.com.uy>
2024-05-04 17:03:05 -0300

    par de cambios

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,54 +1,38 @@
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
     callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        -- Por ahora comentado, porque tiene conflictos con nvim-cmp
-        -- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-        -- Inlay hints - todavía no funca
+        -- Inlay hints
         vim.lsp.inlay_hint.enable()
 
-        -- Signcolumn Icons
+        -- Íconos en los diagnósticos
         local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
         for type, icon in pairs(signs) do
             local hl = "DiagnosticSign" .. type
             vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
         end
 
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        -- Redondito en los diagnósticos
+        vim.diagnostic.config({ virtual_text = { prefix = " ●" } })
+
+        -- Binds personalizadas
         local opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<space>a', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<F3>', function()
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+        -- La predeterminada funca solo en insert
+        vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, opts)
+        -- Formateo para todo el archivo. Para selecciones, usar gq
+        vim.keymap.set("n", "<F3>", function()
             vim.lsp.buf.format { async = true }
         end, opts)
     end,
 })
 
 -- Configuramos los servers para usar cmp
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 require("mason-lspconfig").setup_handlers {
     function(server_name)
--- lua/eze.lua
+++ lua/eze.lua
@@ -8,7 +8,7 @@ vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = false
-vim.opt.colorcolumn = '0'
+vim.opt.colorcolumn = "0"
 -- Tabs
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -35,7 +35,7 @@ vim.opt.fillchars = { fold = " " }
 vim.opt.foldmethod = "indent"
 vim.opt.foldenable = false
 vim.opt.foldlevel = 99
-vim.opt.foldcolumn = '0'
+vim.opt.foldcolumn = "0"
 -- Splits
 vim.opt.splitright = true
 vim.opt.splitbelow = true
@@ -47,7 +47,7 @@ vim.g.netrw_liststyle = 3
 vim.g.netrw_winsize = 30
 vim.g.netrw_preview = 1
 vim.g.netrw_browse_split = 0
-vim.g.netrw_localcopydircmd = 'cp -r'
+vim.g.netrw_localcopydircmd = "cp -r"
 -- Completion menu
 vim.opt.completeopt = "menu,menuone,popup"
 vim.opt.pumblend = 15 -- Transparencia
@@ -56,43 +56,51 @@ vim.opt.pumblend = 15 -- Transparencia
 -- Leader key
 vim.g.mapleader = " "
 -- Borro el resaltado de búsqueda con esc
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Exploro archivos con espacio+e, espacio+l
-vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
-vim.keymap.set('n', '<leader>l', vim.cmd.Lex)
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>l", vim.cmd.Lex)
 -- Me muevo entre buffers
 vim.keymap.set("n", "<Tab>", vim.cmd.bn)
 vim.keymap.set("n", "<S-Tab>", vim.cmd.bp)
 -- Cierro buffers
 vim.keymap.set("n", "<leader>c", vim.cmd.bd)
 -- Mueven lo que esté seleccionado
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ':m ">+1<CR>gv=gv')
+vim.keymap.set("v", "K", ':m "<-2<CR>gv=gv')
 -- Centran la vista mientras me desplazo
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- Reemplaza la palabra bajo el cursor en todo el documento
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- Completado
--- Omnicompletion
-vim.keymap.set("i", "<C-o>", "<C-x><C-o>")
--- File completion
-vim.keymap.set("i", "<C-f>", "<C-x><C-f>")
+-- Omnicompletion - conflicto con nvim-cmp
+-- vim.keymap.set("i", "<C-o>", "<C-x><C-o>")
+-- File completion - conflicto con nvim-cmp
+-- vim.keymap.set("i", "<C-f>", "<C-x><C-f>")
 -- Close completion
 vim.keymap.set("i", "<C-z>", "<C-x><C-z>")
 -- Jump in snippet
-vim.keymap.set({ 'i', 's' }, '<Tab>', function()
+vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
     if vim.snippet.active({ direction = 1 }) then
-        return '<cmd>lua vim.snippet.jump(1)<cr>'
+        return "<cmd>lua vim.snippet.jump(-1)<cr>"
     else
-        return '<Tab>'
+        return "<S-Tab>"
+    end
+end, { expr = true })
+-- Y para el otro lado
+vim.keymap.set({ "i", "s" }, "<Tab>", function()
+    if vim.snippet.active({ direction = 1 }) then
+        return "<cmd>lua vim.snippet.jump(1)<cr>"
+    else
+        return "<Tab>"
     end
 end, { expr = true })
 -- Autocommands
 -- Highlight when yanking (copying) text - Afanado de kickstart.nvim
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
     callback = function()
         vim.highlight.on_yank()
     end,
