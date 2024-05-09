a7e10baf167308f993ddaf6711cb845283de2e9d
Eze <eze.gonzalez@vera.com.uy>
2024-05-09 13:29:51 -0300

    Arreglo bind

--- lua/eze.lua
+++ lua/eze.lua
@@ -66,8 +66,8 @@ vim.keymap.set("n", "<S-Tab>", vim.cmd.bp)
 -- Cierro buffers
 vim.keymap.set("n", "<leader>c", vim.cmd.bd)
 -- Mueven lo que esté seleccionado
-vim.keymap.set("v", "J", ':m ">+1<CR>gv=gv')
-vim.keymap.set("v", "K", ':m "<-2<CR>gv=gv')
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Centran la vista mientras me desplazo
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
