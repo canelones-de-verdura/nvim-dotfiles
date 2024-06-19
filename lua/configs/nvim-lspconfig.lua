c1db99d2079aa4d448e41fdda7a096eaf9143498
Eze <eze.gonzalez@vera.com.uy>
2024-06-19 15:38:07 -0300

    ?

--- lua/configs/nvim-lspconfig.lua
+++ lua/configs/nvim-lspconfig.lua
@@ -6,15 +6,24 @@ vim.api.nvim_create_autocmd("LspAttach", {
         -- Inlay hints
         vim.lsp.inlay_hint.enable()
 
-        -- Íconos en los diagnósticos
-        local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-        end
+        -- Configuración de diagnósticos
+        vim.diagnostic.config({
+            virtual_text = false,
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+            severity_sort = false,
+        })
 
-        -- Redondito en los diagnósticos
-        vim.diagnostic.config({ virtual_text = { prefix = " ●" } })
+        -- Coloreamos el número de línea, pero sin íconos
+        for _, diag in ipairs({ "Error", "Warn", "Info", "Hint" }) do
+            vim.fn.sign_define("DiagnosticSign" .. diag, {
+                text = "",
+                texthl = "DiagnosticSign" .. diag,
+                linehl = "",
+                numhl = "DiagnosticSign" .. diag,
+            })
+        end
 
         -- Binds personalizadas
         local opts = { buffer = ev.buf }
@@ -23,10 +32,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
         vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set({"n", "i"}, "<C-s>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set({ "n", "i" }, "<C-s>", vim.lsp.buf.signature_help, opts)
         vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, opts)
         vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
-        vim.keymap.set({"n", "x"}, "<F3>", function()
+        vim.keymap.set({ "n", "x" }, "<F3>", function()
             vim.lsp.buf.format { async = true }
         end, opts)
     end,
