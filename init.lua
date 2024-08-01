e6f9dab8e3e0eff3338fa3e3ad52cb73634a349b
Eze <eze.gonzalez@vera.com.uy>
2024-08-01 15:40:50 -0300

    ?

--- init.lua
+++ init.lua
@@ -95,6 +95,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     end,
 })
 
+-- Cambiar fondo de la terminal al fondo de Neovim
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if not normal.bg then return end
+    io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+  end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+  callback = function() io.write("\027]111\027\\") end,
+})
+
 --[[ Statusline ]]
 -- Cosas de Git
 local gitbranch = function()
